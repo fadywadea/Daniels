5581a1415301b452411f3536113cdff7e3be135e
fadywadea <fwadea3@gmail.com>
2023-10-16 00:00:22 +0300

    commit

--- js/main.js
+++ js/main.js
@@ -1,11 +1,14 @@
 let navbarBg = document.getElementById("navbar");
+let anchors = document.getElementById("stl-container");
 
 function changeBg() {
-  if (window.scrollY > 716) {
+  if (window.scrollY > 716 - 90) {
     navbarBg.classList.add("nav-scrolled");
+    // anchors.classList.add("stl-logo","stl-logo2");
   }
   else {
     navbarBg.classList.remove("nav-scrolled");
+    // anchors.classList.replace("stl-logo2", "stl-logo");
   }
 
   if (window.scrollY < 0) {
@@ -19,7 +22,7 @@ window.addEventListener("scroll", changeBg);
 $(window).scroll(() => {
   let windowScroll = $(window).scrollTop();
 
-  if (windowScroll > 716) {
+  if (windowScroll > 716 - 90) {
     $('.navigation a').css('color', 'black');
     $('.stl-logo').css('color', 'black');
 
